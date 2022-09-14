5885f4eb17119c61cf43957759cf8d5cefbeaf6b
Dell <kadi.marss@voco.ee>
2022-09-14 21:46:02 +0300

    Repaired and upgraded

--- src/App.js
+++ src/App.js
@@ -1,97 +1,94 @@
-import {React, Component} from "react";
+import { React, Component } from 'react';
+import './App.css';
 
-class App extends Component{
-    constructor() {
+class App extends Component {
+    constructor(){
         super();
-        this.state={
-            title : "CRUD app",
-            employeeData : [],
+        this.state = {
+            title : "CRUD APP",
             act : 0,
-            index : ''
+            idx : '',
+            datas : []
         }
     }
-    handleSubmit= (e) => {
+
+    componentDidMount(){
+        this.refs.txtName.focus();
+    }
+
+    handleSubmit=(e)=>{
         e.preventDefault();
-        let employeeData = this.state.employeeData;
+        let datas = this.state.datas;
         let name = this.refs.txtName.value;
         let age = this.refs.txtAge.value;
 
         if(this.state.act === 0)
         {
-            let newEmployee = {
+            let data = {
                 "name" : name,
                 "age" : age
             }
-            employeeData.push(newEmployee);
+            datas.push(data);
         }
         else
         {
-            let index = this.state.index;
-            employeeData[index].name = name;
-            employeeData[index].age = age;
+            let index = this.state.idx;
+            datas[index].name = name;
+            datas[index].age = age;
         }
 
         this.setState({
-            employeeData: employeeData,
+            datas : datas,
             act : 0
         })
         this.refs.myForm.reset();
+        this.refs.txtName.focus();
     }
 
-    handleEdit = (i) =>{
-        let employeeData = this.state.employeeData[i];
-        this.refs.txtName.value = employeeData.name;
-        this.refs.txtAge.value = employeeData.age;
-
+    handleDelete = (index) =>{
+        let datas = this.state.datas;
+        datas.splice(index,1);
         this.setState({
-            employeeData : employeeData,
-            act : 1,
-            index : i
+            datas:datas
         })
-
+        this.refs.txtName.focus();
     }
 
-    handleDelete= (i) =>{
-        let employeeData = this.state.employeeData;
-        employeeData.splice(i, 1);
-        this.set.State({
-            employeeData : employeeData
-        });
+    handleEdit = (index) => {
+        let data = this.state.datas[index];
+        this.refs.txtName.value = data.name;
+        this.refs.txtAge.value = data.age;
+        this.setState({
+            act: 1,
+            idx : index
+        })
     }
 
     render() {
-        let employeeData = this.state.employeeData;
-        return(
-            <div>
-                <h1>{this.setState.title}</h1>
-                <form ref="myForm">
-                <label>Name</label>
-                <input type="text" ref="txtName"/>
-                <label>Age</label>
-                <input type="text" ref="txtAge"/>
-                <button onClick={e => this.handleSubmit(e)}> Save</button>
-            </form>
-            <tabel>
-                <tr>
-                    <th>Name</th>
-                    <th>Age</th>
-                </tr>
-                {
-                    employeeData.map( (data, i) =>
-                    <tr key={i}>
-                        <td>{data.name}</td>
-                        <td>{data.age}</td>
-                        <td>
-                            <button onClick={this.handleEdit(i)}>Edit</button>
-                        </td>
-                        <td>
-                            <button onClick={this.handleDelete(i)}>Delete</button>
-                        </td>
-                    </tr> )
-                }
-            </tabel>
+        let datas = this.state.datas;
+        return (
+            <div className="App">
+
+                <form ref="myForm" className="form">
+                    <h1>{this.state.title}</h1>
+                    <label>Name</label>
+                    <input type="text" ref="txtName" className="field"/>
+                    <label>Age</label>
+                    <input type="text" ref="txtAge"  className="field"/>
+                    <button onClick={e => this.handleSubmit(e)} className="button"> Save</button>
+                </form>
+                <pre className="list">
+          {datas.map((data,index)=>
+              <li key={index}>
+                  {index+1}, {data.name} ,{data.age}
+                  <button onClick={e => this.handleDelete(index)} className="listbutton">Delete</button>
+                  <button onClick={e => this.handleEdit (index)} className="listbutton">Edit</button>
+              </li>
+          )
+          }
+        </pre>
             </div>
-        )
+        );
     }
 }
 
