408852081ecfc417759f6ae75fc5b0574548dbea
Dell <kadi.marss@voco.ee>
2022-09-14 20:22:29 +0300

    As a user I can see a list of #183267912

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
-import { React , Component } from 'react';
+import {React, Component} from "react";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {}
-  }
-  render() {
-    let datas = this.state.datas;
-    return (
-        <form>
-          <label>Name</label>
-          <input type="text" ref="txtName"/>
-          <label>Age</label>
-          <input type="text" ref="txtAge"/>
-          <button> Save</button>
-        </form>
-    )
-  }
+class App extends Component{
+    constructor() {
+        super();
+        this.state={
+            employeeData : []
+        }
+    }
+    handleSubmit= (e) => {
+        e.preventDefault();
+        let employeeData = this.state.employeeData;
+        let name = this.refs.txtName.value;
+        let age = this.refs.txtAge.value;
+
+        let newEmployee = {
+            "name" : name,
+            "age" : age
+        }
+        employeeData.push(newEmployee);
+        this.setState({
+            employeeData: employeeData
+        })
+        this.refs.myForm.reset();
+    }
+
+    render() {
+        let employeeData = this.state.employeeData;
+        return(
+            <div>
+            <form ref="myForm">
+                <label>Name</label>
+                <input type="text" ref="txtName"/>
+                <label>Age</label>
+                <input type="text" ref="txtAge"/>
+                <button onClick={e => this.handleSubmit(e)}> Save</button>
+            </form>
+            <tabel>
+                <tr>
+                    <th>Name</th>
+                    <th>Age</th>
+                </tr>
+                {
+                    employeeData.map( (data, i) =>
+                    <tr key={i}>
+                        <td>{data.name}</td>
+                        <td>{data.age}</td>
+                    </tr> )
+                }
+            </tabel>
+            </div>
+        )
+    }
 }
+
+export default App;
